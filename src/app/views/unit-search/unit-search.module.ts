ee989d735cc21f8f06a7e766f9c1148c356b6078
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2023-10-20 13:10:28 -0300

    feat: layout legend

--- src/app/views/unit-search/unit-search.module.ts
+++ src/app/views/unit-search/unit-search.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UnitSearchComponent } from './unit-search.component';
 import { FormComponent } from '../form/form.component';
+import { LegendComponent } from '../legend/legend.component';
 
 @NgModule({
   declarations: [
     UnitSearchComponent,
-    FormComponent
+    FormComponent,
+    LegendComponent
   ],
   imports: [
     CommonModule,
