31a82217ec81298dfd4fab3dd25e8acbdf59ebe7
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2023-10-24 12:01:11 -0300

    feat: get total filter

--- src/app/views/form/form.component.ts
+++ src/app/views/form/form.component.ts
@@ -1,43 +1,48 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { IOptions } from 'src/app/models/options';
+import { StateService } from 'src/app/services/state.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
 
   @Output() formSearch = new EventEmitter();
 
   form = this.formBuilder.group({
     hour: ['', [Validators.required]],
-    opened: [false]
+    opened: [false],
+    day: [new Date().getDay()]
   })
 
   options: IOptions[] = [
     {
       title: 'Manhã',
       hour: '06:00 às 12:00',
-      start: '06:00',
-      end: '12:00',
     },
     {
       title: 'Tarde',
       hour: '12:01 às 18:00',
-      start: '12:01',
-      end: '18:00',
     },
     {
       title: 'Noite',
       hour: '18:01 às 23:00',
-      start: '18:01',
-      end: '23:00',
     }
-  ]
+  ];
 
-  constructor(private formBuilder: FormBuilder){}
+  total = 0;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private state: StateService
+    ){}
+
+  ngOnInit(): void {
+    this.state.getFilterData().subscribe(res => this.total = res.length);
+  }
 
   search(){
     this.formSearch.emit(this.form.value)
